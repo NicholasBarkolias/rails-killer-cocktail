4c67c673ed32d8d958ddbc70d6791c5bc9cc4daf
Nicholas Barkolias <nbarkolias@gmail.com>
2020-09-01 23:26:12 +1000

    app copied over

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,14 @@
 class Cocktail < ApplicationRecord
+  validates :name, presence: true
+  validates_uniqueness_of :name
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
+
+  def self.search(search)
+    where("name LIKE ?", "%#{search.capitalize}%")
+  end
+
+  def name=(s)
+    super s.titlecase
+  end
 end
