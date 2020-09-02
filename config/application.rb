b3d31262bf66bc03651a50da8e2c45f3419b4d64
Nicholas Barkolias <nbarkolias@gmail.com>
2020-09-02 18:30:56 +1000

    precompile

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module RailsKillerCocktail
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.sass
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
