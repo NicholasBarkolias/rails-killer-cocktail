98c04f4ccf5adb43bbc6ad8cfc5fd840dc40a4d2
Nicholas Barkolias <nbarkolias@gmail.com>
2020-09-03 21:30:46 +1000

    photo image preview

--- app/javascript/components/photo_preview.js
+++ app/javascript/components/photo_preview.js
@@ -1,9 +1,12 @@
 const previewImageOnFileSelect = () => {
   // we select the photo input
+
+  console.log('hello')
   const input = document.getElementById('photo-input');
   if (input) {
     // we add a listener to know when a new picture is uploaded
     input.addEventListener('change', () => {
+
       // we call the displayPreview function (who retrieve the image url and display it)
       displayPreview(input);
     })
